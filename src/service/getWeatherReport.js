949ec03c797fde925ae2f6dc6f3d8cd84df51c7e
PrashantThakurNitP <prashant09.thakur@gmail.com>
2024-01-19 22:46:54 +0530

    url update

--- src/service/getWeatherReport.js
+++ src/service/getWeatherReport.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 export const getWeatherReport = async (city) => {
-  const apiUrl = "http://localhost:8080/v1/weather";
+  const apiUrl = "http://localhost:8088/v1/weather";
 
   try {
     const response1 = await axios.get(apiUrl, { params: { cityName: city } });
     console.log(response1);
 
-    const response2 = await axios.get("http://localhost:8086/v1/weather", {
-      params: { cityName: city },
-    });
-    console.log(response2);
+    // const response2 = await axios.get("http://localhost:8086/v1/weather", {
+    //   params: { cityName: city },
+    // });
+    // console.log(response2);
 
     return response1;
   } catch (error) {
