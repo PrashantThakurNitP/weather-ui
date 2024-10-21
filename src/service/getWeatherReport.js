7ff60b4fb63aaf2eeebf1882f0ead531db899953
PrashantThakurNitP <prashant09.thakur@gmail.com>
2024-10-21 15:40:20 +0530

    log

--- src/service/getWeatherReport.js
+++ src/service/getWeatherReport.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getWeatherReport = async (city) => {
   const apiUrl = `${process.env.REACT_APP_WEATHER_SERVICE_API_URL}${process.env.REACT_APP_WEATHER_SERVICE_GET_WEATHER_ENDPOINT}`;
-
+  console.log("Url", apiUrl);
   try {
     const response = await axios.get(apiUrl, {
       params: { cityName: city, days: 3 },
